5e78da2d5510303be443eefa411ae47fbe2b555e
FJH6100 <39116989+FJH6100@users.noreply.github.com>
2022-01-17 00:14:45 -0600

    Added Player Movement

---
+++ 2.5DPlatformer/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5f;
+    [SerializeField]
+    private float _gravity = 9.8f;
+    [SerializeField]
+    private float _jumpHeight = 15f;
+    private float _yVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontal, 0, 0);
+        Vector3 velocity = direction * _speed;
+        if (_controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+            }
+        }
+        else
+        {
+            _yVelocity -= _gravity * Time.deltaTime;
+        }
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
